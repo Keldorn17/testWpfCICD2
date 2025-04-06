93315bf3393ec77b1fad63c10ad04dfb569fb131
Keldorn17 <patai17.zoltan17@gmail.com>
2025-04-06 10:45:03 +0200

    handle

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,5 +9,30 @@ namespace TODO;
 /// </summary>
 public partial class App : Application
 {
+    public App()
+    {
+        this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+    }
+
+    private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+    {
+        MessageBox.Show($"Unhandled exception: {e.Exception.Message}");
+        e.Handled = true; // Prevent the application from crashing
+    }
+
+    [STAThread]
+    public static void Main()
+    {
+        try
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Error: {ex.Message}");
+        }
+    }
 }
 
